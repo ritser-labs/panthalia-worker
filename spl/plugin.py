6b24f313880a266627a9b5bf982dba424fd3a45e
ia03 <ibrahim@ibrahimahmed.ca>
2024-09-11 12:42:49 -0400

    increase LR

--- spl/plugin.py
+++ spl/plugin.py
@@ -46,7 +46,7 @@ class StandardPlugin:
         tokenizer,
         num_microbatches,
         example_per_microbatch,
-        max_lr=0.001,
+        max_lr=0.0002,
         min_lr=0.0001,
         tensor_version_interval=43,
         expected_worker_time=40
