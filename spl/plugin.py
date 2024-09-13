92fcd6fcef4a1f72f0c13ec1d4327c748645b037
ia03 <ibrahim@ibrahimahmed.ca>
2024-09-12 23:10:38 -0400

    reduce batch size

--- spl/plugin.py
+++ spl/plugin.py
@@ -106,9 +106,9 @@ class StandardPlugin:
             'accumulation_steps': self.accumulation_steps
         }
 
-NUM_MICROBATCHES = 512 # 256
+NUM_MICROBATCHES = 64 # 256,512
 
-EXAMPLES_PER_MICROBATCH = 512 # 32
+EXAMPLES_PER_MICROBATCH = 512 # 32,512
 
 exported_plugin = StandardPlugin(
     model_adapter,
