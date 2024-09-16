2b076bf990db688efb835e7a4cee0625de7ca172
Ibrahim Ahmed <ibrahim@ibrahimahmed.ca>
2024-09-16 16:11:24 -0400

    update num_microbatches

--- spl/plugin.py
+++ spl/plugin.py
@@ -94,7 +94,7 @@ model_adapter = NanoGPTModelAdapter(model_config)
 
 dataset = ShakespeareDataLoader(model_config, buffer_size=100_000, max_seq_len=model_params.block_size)
 
-NUM_MICROBATCHES = 200
+NUM_MICROBATCHES = 50
 
 EXAMPLES_PER_MICROBATCH = 512
 
