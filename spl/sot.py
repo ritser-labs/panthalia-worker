25c366ec4f776d47c76029cd656226b6704e1d36
ia03 <ibrahim@ibrahimahmed.ca>
2024-09-20 01:24:57 -0400

    fix torch.save calls to thread in sot.py

--- spl/sot.py
+++ spl/sot.py
@@ -623,7 +623,7 @@ def create_app(public_keys_file, enable_memory_logging=False):
 
             # Update the accumulated_grads tensor
             accumulated_grads += tensor.to(device)
-            torch.save(accumulated_grads, accumulated_grads_path)
+            await asyncio.to_thread(torch.save, accumulated_grads, accumulated_grads_path)
 
             # Calculate the future tensor
             current_version_number = block_timestamps.get(tensor_name, 0)
@@ -652,8 +652,8 @@ def create_app(public_keys_file, enable_memory_logging=False):
             future_tensor_adam_m_temp_path = os.path.join(state_dir, f'{tensor_name}_adam_m_{future_version_number}.pt.tmp')
 
             # Save the future tensor and momentum to temporary files
-            asyncio.to_thread(torch.save, future_tensor, future_tensor_temp_path)
-            asyncio.to_thread(torch.save, m_update, future_tensor_adam_m_temp_path)
+            await asyncio.to_thread(torch.save, future_tensor, future_tensor_temp_path)
+            await asyncio.to_thread(torch.save, m_update, future_tensor_adam_m_temp_path)
 
             # Atomically rename the temporary files to their final names
             os.rename(future_tensor_temp_path, future_tensor_path)
