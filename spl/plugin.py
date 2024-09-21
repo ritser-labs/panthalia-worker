78a927f060571a57d323aeba71cd3f5082d60b4f
ia03 <ibrahim@ibrahimahmed.ca>
2024-09-21 11:43:54 -0400

    revert plugin changes

--- spl/plugin.py
+++ spl/plugin.py
@@ -26,7 +26,7 @@ model_adapter = NanoGPTModelAdapter(model_config)
 
 dataset = FineWebDataLoader(model_config, buffer_size=100, max_seq_len=model_params.block_size)
 
-NUM_STEPS = 1500
+NUM_STEPS = 500
 
 EXAMPLES_PER_STEP = 14
 
@@ -40,12 +40,12 @@ exported_plugin = StandardPlugin(
     outer_max_lr=1,
     outer_min_lr=1,
     outer_weight_decay=0.0,
-    tensor_version_interval=665,
-    expected_worker_time=650,
-    max_concurrent_iterations=8,
+    tensor_version_interval=250,
+    expected_worker_time=240,
+    max_concurrent_iterations=4,
     inner_max_lr=6e-4,
     inner_min_lr=6e-4,
-    inner_T_0=400,
+    inner_T_0=200,
     inner_weight_decay=0.1,
     preload_batch_count=4
 )
