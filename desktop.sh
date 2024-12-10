2e734f861b4fe9ea4d178a987926c498ab00969f
ia03 <ibrahim@ibrahimahmed.ca>
2024-12-09 23:32:40 -0500

    remove sot_url arg

--- desktop.sh
+++ desktop.sh
@@ -1,1 +1,1 @@
- python3 -m spl.deploy.local_test_run --sot_url http://localhost:5001 --private_key $PRIVATE_KEY --worker_count 1 --torch_compile
+ python3 -m spl.deploy.local_test_run --private_key $PRIVATE_KEY --worker_count 1 --torch_compile
